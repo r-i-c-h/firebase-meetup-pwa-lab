eeff4a9b2476cd7eff6070a85edd774e736f40bf
r-i-c-h <rich@richwerden.com>
2020-06-24 00:18:33 -0700

    Adds Yes/No Attending btns

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Import stylesheets
-import './style.css';
+import "./style.css";
 // Firebase App (the core Firebase SDK) is always required and must be listed first
 import * as firebase from "firebase/app";
 
@@ -7,33 +7,33 @@ import * as firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
 
-import * as firebaseui from 'firebaseui';
+import * as firebaseui from "firebaseui";
 
 // Document elements
 
-const guestbookContainer = document.getElementById('guestbook-container');
-const startRsvpButton = document.getElementById('startRsvp');
+const guestbookContainer = document.getElementById("guestbook-container");
+const startRsvpButton = document.getElementById("startRsvp");
 
-const form = document.getElementById('leave-message');
-const input = document.getElementById('message');
-const guestbook = document.getElementById('guestbook');
-const numberAttending = document.getElementById('number-attending');
-const rsvpYes = document.getElementById('rsvp-yes');
-const rsvpNo = document.getElementById('rsvp-no');
+const form = document.getElementById("leave-message");
+const input = document.getElementById("message");
+const guestbook = document.getElementById("guestbook");
+const rsvpYes = document.getElementById("rsvp-yes");
+const rsvpNo = document.getElementById("rsvp-no");
+const numberAttending = document.getElementById("number-attending");
 
 var rsvpListener = null;
 var guestbookListener = null;
 
 // Add Firebase project configuration object here
-  var firebaseConfig = {
-    apiKey: "AIzaSyA_lIEOse2oFqB_ibAI_0__vN8dvCJl_p0",
-    authDomain: "fir-meetuppwa-codelab.firebaseapp.com",
-    databaseURL: "https://fir-meetuppwa-codelab.firebaseio.com",
-    projectId: "fir-meetuppwa-codelab",
-    storageBucket: "fir-meetuppwa-codelab.appspot.com",
-    messagingSenderId: "268437799300",
-    appId: "1:268437799300:web:8a51b86d3c18f03715ddd1"
-  };
+var firebaseConfig = {
+  apiKey: "AIzaSyA_lIEOse2oFqB_ibAI_0__vN8dvCJl_p0",
+  authDomain: "fir-meetuppwa-codelab.firebaseapp.com",
+  databaseURL: "https://fir-meetuppwa-codelab.firebaseio.com",
+  projectId: "fir-meetuppwa-codelab",
+  storageBucket: "fir-meetuppwa-codelab.appspot.com",
+  messagingSenderId: "268437799300",
+  appId: "1:268437799300:web:8a51b86d3c18f03715ddd1"
+};
 
 firebase.initializeApp(firebaseConfig);
 
@@ -45,7 +45,7 @@ const uiConfig = {
     firebase.auth.EmailAuthProvider.PROVIDER_ID
   ],
   callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl){
+    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
       // Handle sign-in.
       // Return false to avoid redirect.
       return false;
@@ -53,68 +53,90 @@ const uiConfig = {
   }
 };
 
-
 const ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-startRsvpButton.addEventListener("click",
- (e) => {
-    if (firebase.auth().currentUser) {   // User is signed in; allows user to sign out
-      firebase.auth().signOut();
-    } else {
-      ui.start("#firebaseui-auth-container", uiConfig); // No user is signed in; allows user to sign in
-    }
-});
-
 // SUBSCRIBE to the CURRENT AUTH STATE
-firebase.auth().onAuthStateChanged((user)=> {
-if (user){
-   startRsvpButton.textContent = "LOGOUT";
-   // Show guestbook to logged-in users
-   guestbookContainer.style.display = "block";
-   subscribeToGuestBook();
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+    startRsvpButton.textContent = "LOGOUT";
+    // Show guestbook to logged-in users
+    guestbookContainer.style.display = "block";
+    subscribeToGuestBook();
   } else {
-    startRsvpButton.textContent = "RSVP"
+    startRsvpButton.textContent = "RSVP";
     guestbookContainer.style.display = "none";
     unsubscribeToGuestbook();
   }
 });
 
+function subscribeToGuestBook() {
+  guestBookListener = firebase
+    .firestore()
+    .collection("guestbook")
+    .orderBy("timestamp", "desc")
+    .onSnapshot(snaps => {
+      // returns an UNSUBSCRIBE function.
+      // Reset page
+      guestbook.innerHTML = "";
+      // Loop through documents in database
+      snaps.forEach(doc => {
+        // Create an HTML entry for each document and add it to the DOM
+        const entry = document.createElement("p");
+        entry.textContent = doc.data().name + ": " + doc.data().text;
+        guestbook.appendChild(entry);
+      });
+    });
+}
+
+function unsubscribeToGuestbook() {
+  if (guestbookListener != null) {
+    guestbookListener();
+    guestbookListener = null;
+  }
+}
+
+startRsvpButton.addEventListener("click", e => {
+  if (firebase.auth().currentUser) {
+    // User is signed in; allows user to sign out
+    firebase.auth().signOut();
+  } else {
+    ui.start("#firebaseui-auth-container", uiConfig); // No user is signed in; allows user to sign in
+  }
+});
 // Listen to the form submission
-form.addEventListener("submit", (e) => {
- // Prevent the default form redirect
- e.preventDefault();
- // Write a new message to the database collection "guestbook"
- firebase.firestore().collection("guestbook").add({
-   text: input.value,
-   timestamp: Date.now(),
-   name: firebase.auth().currentUser.displayName,
-   userId: firebase.auth().currentUser.uid
- })
- // clear message input field
- input.value = ""; 
- // Return false to avoid redirect
- return false;
+form.addEventListener("submit", e => {
+  // Prevent the default form redirect
+  e.preventDefault();
+  // Write a new message to the database collection "guestbook"
+  firebase
+    .firestore()
+    .collection("guestbook")
+    .add({
+      text: input.value,
+      timestamp: Date.now(),
+      name: firebase.auth().currentUser.displayName,
+      userId: firebase.auth().currentUser.uid
+    });
+  // clear message input field
+  input.value = "";
+  // Return false to avoid redirect
+  return false;
 });
 
-function subscribeToGuestBook(){
-  const guestBookListener = firebase.firestore().collection("guestbook")
-  .orderBy("timestamp","desc")
-  .onSnapshot((snaps) => {      // returns an UNSUBSCRIBE function.
-    // Reset page
-    guestbook.innerHTML = "";
-    // Loop through documents in database
-    snaps.forEach((doc) => {
-      // Create an HTML entry for each document and add it to the DOM
-      const entry = document.createElement("p");
-      entry.textContent = doc.data().name + ": " + doc.data().text;
-      guestbook.appendChild(entry);
-    });
-  });
-  }
+rsvpYes.addEventListener('click', () => {
+ // Get a reference to the user's document in the attendees collection
+ const userDoc = firebase.firestore().collection('attendees').doc(firebase.auth().currentUser.uid);
 
-  function unsubscribeToGuestbook(){
-    if (guestbookListener != null) {
-      guestbookListener();
-      guestbookListener = null;
-    }
-  };
+ // If they RSVP'd yes, save a document with attending: true
+ userDoc.set({
+   attending: true
+ }).catch(console.error)
+});
+
+rsvpNo.addEventListener('click', () => {
+ const userDoc = firebase.firestore().collection('attendees').doc(firebase.auth().currentUser.uid);
+ userDoc.set({
+   attending: false
+ }).catch(console.error)
+});
+
